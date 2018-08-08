29a350a01133fcc7406de85af04f18b59953c723
JAJAKUL <jajakulislam@gmail.com>
2018-08-08 05:50:11 +0530

    initial commit

---
+++ firstclass.py
@@ -0,0 +1,35 @@
+"""print ("hello world")
+help()
+import os
+os.system('cls') 
+# is a single line command
+"""multi line command"""
+
+'''kdjojsoj
+sdkhsh
+jsldjs
+'''
+'\nkdjojsoj\nsdkhsh\njsldjs\n'
+
+
+>>> x="hello world"
+>>> x
+'hello world'
+>>> x+"hello python"
+'hello worldhello python'
+>>>
+
+"""
+>>> string_way1='hello world'
+>>> string_way1
+'hello world'
+>>> string_way2="hello world"
+>>> string_way2
+'hello world'
+>>> string_way3='''hello world'''
+>>> string_way3
+'hello world'
+>>>
+#none is key word on python
+
+#What are deffrent type of key word on python?
