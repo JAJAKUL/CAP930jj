fdbbc6d6177940b2f5d41cca49ecc0c29f1a32ab
JAJAKUL <jajakulislam@gmail.com>
2018-08-10 14:41:02 +0530

    initial commit

--- class note/4thclass.py
+++ class note/4thclass.py
@@ -1,3 +1,5 @@
+'''
+
 print('spam eggs')
 print('doesn\'t')
 print("doesn't")
@@ -16,7 +18,7 @@ print(isinstance(None,object))
 print(isinstance([1,2,3],object))
 #id(object) gives object's "identity".#
 print(id(object))
-'''
+
 "identity " is unique and fixed during an object's lifetime object are tagged with their type at run time
 objects contain pointers to their data blob.
 this means even small things take up a lot of space!
@@ -26,7 +28,7 @@ Variable are refrence to objects.
 
 
 
-'''
+
 
 print((64).__sizeof__())
 
@@ -39,3 +41,37 @@ obj2=compute([1],[2,3],3)
 print(obj2)
 obj3=compute('l','olo',4)
 print(obj3)
+
+
+print()
+
+print(1==1.0)
+'''
+print(1==1.0)
+print(type(1)!=type(1.0))
+gree="hello world!"
+print(gree)
+print(gree[4])
+print('world' in gree)
+print(len(gree))
+#find the letter position#
+print(gree.find('lo'))
+#replace the value#
+print(gree.replace('llo','y'))
+#find the name and return true or false value#
+print(gree.startswith('hell'))
+print(gree.isalpha())
+
+print()
+print(gree.lower())
+print(gree.title())
+print(gree.upper())
+print(gree.strip())
+print(gree.strip('h'))
+#convert the string into a list just use the split() function #
+print('ham sath sath hai'.split())
+print('10-08-2018'.split(sep='-'))
+print('10/08/2018'.split(sep='/'))
+print(','.join(['Eric','John']))
+
+
